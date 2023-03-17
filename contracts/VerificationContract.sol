571ebad6e4a34c09996643b31425f1f828a0d4bb
Howie9494 <Winnie.i0106>
2023-03-17 19:22:24 +0000

    Add storage for contractId in logContract

--- contracts/VerificationContract.sol
+++ contracts/VerificationContract.sol
@@ -18,11 +18,11 @@ contract VerificationContract {
         dataUsageContract = DataUsageContract(_dataUsageContract);
     }
 
-    function verify(uint _logId, bytes32 _contractId) public view returns(address) {
+    function verify(uint _logId) public view returns(address) {
         // 获取日志信息
-        (address logActorId,Operator logOperation, string[] memory processedData) = logContract.getLog(_logId);
+        (address logActorId,Operator logOperation, string[] memory processedData,bytes32 contractId) = logContract.getLog(_logId);
         // （1）检查行为人是否符合协议
-        (address agreementActorAddress,bool userConsent) = agreementContract.getVote(_contractId);
+        (address agreementActorAddress,bool userConsent) = agreementContract.getVote(contractId);
 
         if (agreementActorAddress != logActorId) {
             return logActorId;
@@ -30,7 +30,7 @@ contract VerificationContract {
 
         // 日志合同所记录的每个行为者的操作是否符合那些通过数据使用合同记录并通过协议合同获得用户同意的操作
         // 检查操作是否符合数据使用合同 && 检查该行为人是否已经在ProtocolContract中记录了同意
-         (Operator dataUseOperation, string[] memory personalDataList) = dataUsageContract.getActor(_contractId);
+         (Operator dataUseOperation, string[] memory personalDataList) = dataUsageContract.getActor(contractId);
         if ((dataUseOperation != logOperation) || (!userConsent)) {
             return logActorId;
         } 
