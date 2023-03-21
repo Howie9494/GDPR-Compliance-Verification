4f651ea26906ee08e84d7f5d3b286540ab448ede
Clarisse Lee <128330856+ClarisseLee@users.noreply.github.com>
2023-03-21 02:14:22 +0000

    Update DataUsageContractTest.sol
    review codes, update the right code.

--- Test/DataUsageContractTest.sol
+++ Test/DataUsageContractTest.sol
@@ -3,47 +3,48 @@ pragma solidity ^0.8.0;
 
 import "remix_tests.sol"; // this import is automatically injected by Remix.
 import "../contracts/DataUsageContract.sol";
+import "../contracts/VerificationContract.sol";
 
 contract DataUsageContractTest {
 
     DataUsageContract contractToTest;
+    VerificationContract verificationContract;
 
-    function beforeEach() public {
-        contractToTest = new DataUsageContract();
+    constructor(address _dataUsageContract, address _verificationContract) {
+        contractToTest = DataUsageContract(_dataUsageContract);
+        verificationContract = VerificationContract(_verificationContract);
     }
 
+    string[] personalDataList;
     function addPurposeTest() public {
-        string memory serviceName = "test service";
-        string memory servicePurpose = "test purpose";
-        string[] memory personalDataList = new string[](1);
-        personalDataList[0] = "test data";
-        
+        string memory serviceName = "banking";
+        string memory servicePurpose = "transaction monitoring";
+        personalDataList = ["310 456 789", "2023-03-20 : 30 pounds"];
+
         // Declare and assign a value to purposeId
-        bytes32 purposeId = contractToTest.addPurpose(serviceName, servicePurpose, Operator.WRITE, personalDataList);
+        bytes32 purposeId = contractToTest.addPurpose(msg.sender, serviceName, servicePurpose, Operator.WRITE, personalDataList);
         
         bytes32 expectedId = keccak256(abi.encode(msg.sender, serviceName, servicePurpose, Operator.WRITE));
+        // bytes32 expectedId = keccak256(abi.encode(address(this), serviceName, servicePurpose, Operator.WRITE));
+        // Assert.equal(purposeId, address(this), "Purpose ID is incorrect");
         Assert.equal(purposeId, expectedId, "Purpose ID is incorrect");
     }
 
     function getPurposeTest() public {
-        string memory serviceName = "test service";
-        string memory servicePurpose = "test purpose";
-        string[] memory personalDataList = new string[](1);
-        personalDataList[0] = "test data";
-        bytes32 purposeId = contractToTest.addPurpose(serviceName, servicePurpose, Operator.WRITE, personalDataList);
-
+        string memory serviceName = "banking";
+        string memory servicePurpose = "transaction monitoring";
+        personalDataList = ["310 456 789", "2023-03-20 : 30 pounds"];
+       
         // Test for the existence of the purpose
-        bytes32 receivedPurposeId = purposeId;
         bytes32 expectedPurposeId = keccak256(abi.encode(msg.sender, serviceName, servicePurpose, Operator.WRITE));
-        Assert.equal(receivedPurposeId, expectedPurposeId, "Purpose ID is incorrect");
+        Assert.equal(contractToTest.addPurpose(msg.sender, serviceName, servicePurpose, Operator.WRITE, personalDataList), expectedPurposeId, "Purpose ID is incorrect");
 
         // Test the details of the purpose
-        (Operator operator, string[] memory storedData) = contractToTest.getPurpose(purposeId);
+        (Operator operator, string[] memory storedData) = contractToTest.getPurpose(contractToTest.addPurpose(msg.sender, serviceName, servicePurpose, Operator.WRITE, personalDataList));
         uint receivedOperator = uint(operator);
         uint expectedOperator = uint(Operator.WRITE);
-        Assert.equal(receivedOperator, expectedOperator, "Operator is incorrect");
-        Assert.equal(storedData.length, 1, "Data list length is incorrect");
-        Assert.equal(storedData[0], "test data", "Stored data is incorrect");
+        Assert.equal(receivedOperator, expectedOperator, "Operator is correct");
+        Assert.equal(storedData.length, 1, "Data list length is correct");
+        Assert.equal(verificationContract.LoopCompare(storedData, personalDataList), true, "Stored data is correct");
     }
-
 }
